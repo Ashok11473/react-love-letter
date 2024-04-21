4db364bc5f8b73e3ebcdd9752be69ba5e35b295f
Ashok11473 <141929568+Ashok11473@users.noreply.github.com>
2024-04-20 21:06:27 -0400

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,7 +35,7 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma Deepu🖤,<br />
+        My dear kundanapu bomma Deepu🖤,<br />
         never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
         always yours,<br />
         @Ashok
